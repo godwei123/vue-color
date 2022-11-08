c737d1063432e5cf28bd23c52474172eb42f52fe
godwei123 <1374532240@qq.com>
2022-11-08 22:02:28 +0800

    CompactPicker

--- packages/components/compact/CompactFields.vue
+++ packages/components/compact/CompactFields.vue
@@ -15,6 +15,7 @@
 
 import EditableInput from "../../common/EditableInput.vue";
 import {ColorObject} from "../../interface";
+import {isValid, toHex} from "../../utils/color";
 
 interface FieldsPropsType {
   color: ColorObject
@@ -29,16 +30,7 @@ const styles = {
   fontSize: '14px'
 }
 
-const toHex = (val: number) => {
-  if (val < 0) return '00';
-  if (val > 255) return 'ff'
-  const res = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
-  return `${res[Math.floor(val / 16)]}${res[val % 16]}`;
-}
-const isValid = (val: unknown) => {
-  const set = new Set<string>(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'a', 'b', 'c', 'd', 'e', 'f'])
-  return String(val).split("").every(it => set.has(it))
-}
+
 const change = (value: string, type: string | undefined) => {
   if (type !== undefined && !isValid(value)) {
     return
--- packages/utils/color.ts
+++ packages/utils/color.ts
@@ -114,3 +114,15 @@ export const convertColor = (color: ColorInput): ColorObject => {
 export const formatColor = (color: ColorObject | string, format: ColorFormat) => {
     return tinycolor(typeof color === 'string' ? color : color.origin).toString(format);
 }
+
+
+export const toHex = (val: number) => {
+    if (val < 0) return '00';
+    if (val > 255) return 'ff'
+    const res = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
+    return `${res[Math.floor(val / 16)]}${res[val % 16]}`;
+}
+export const isValid = (val: unknown) => {
+    const set = new Set<string>(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'a', 'b', 'c', 'd', 'e', 'f'])
+    return String(val).split("").every(it => set.has(it))
+}
